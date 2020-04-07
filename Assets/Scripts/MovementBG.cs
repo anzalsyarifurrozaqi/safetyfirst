97048a96453a5a30578bb1d3d1a1fff107156085
farisalka <muhammadfarisalkautsar@gmail.com>
2020-04-07 08:33:34 +0700

    Edit UI Menu Credit

---
+++ Assets/Scripts/MovementBG.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementBG : MonoBehaviour
+{
+    public float speed = 1f; // for BG speed movements
+
+    public float clamppos; // for clamping position
+
+    public Vector3 StartPosition; // get our first position
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float NewPosition = Mathf.Repeat(Time.time * speed , clamppos);
+        transform.position = StartPosition + Vector3.left * NewPosition;
+    }
+}
